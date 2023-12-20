5d74659f9476c0c6c4bd2093470cd8a6f27a9acb
ingeloflame <149815620+ingeloflame@users.noreply.github.com>
2023-12-21 03:53:23 +0500

    Update test_pytest.py

--- MainApp/test_pytest.py
+++ MainApp/test_pytest.py
@@ -6,12 +6,12 @@ class Def_test(TestCase):
    def test_translator(self):
       article_en = "This is a test article in English"
       translated = translator(article_en)
-      self.assertEqual(translated, "Это тестовая статья на английском языке")
+      self.assertEqual(translated, "Это тест статья на английском языке")
 
    def test_summarizer(self):
       article_text = "Это тестовая статья на русском языке. Она содержит некоторый текст для проверки функции summarizer."
       summary = summarizer(article_text)
-      self.assertEqual(summary, "Тестовая статья на русском языке. Проверка функции summarizer.")
+      self.assertEqual(summary, "Представляю тестовую статью на русском языке, которая содержит некоторый текст для проверки функции summarizer.")
 
 
 
