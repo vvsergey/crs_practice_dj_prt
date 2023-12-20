65983914cd5ddbab7262827a921116c9509de0ff
ingeloflame <149815620+ingeloflame@users.noreply.github.com>
2023-12-21 03:23:31 +0500

    Update test_pytest.py

--- MainApp/test_pytest.py
+++ MainApp/test_pytest.py
@@ -3,19 +3,15 @@ from hf.py import translator
 from hf.py import summarizer
 
 def test_translator():
-article_en = "This is a test article in English"
-translated = translator(article_en)
-assert translated == "Это тестовая статья на русском языке"
+    article_en = "This is a test article in English"
+    translated = translator(article_en)
+    assert translated == "Это тестовая статья на русском языке"
 
 def test_summarizer():
-article_text = "Это тестовая статья на русском языке. Она содержит некоторый текст для проверки функции summarizer."
-summary = summarizer(article_text)
-assert summary == "Тестовая статья на русском языке. Проверка функции summarizer."
+    article_text = "Это тестовая статья на русском языке. Она содержит некоторый текст для проверки функции summarizer."
+    summary = summarizer(article_text)
+    assert summary == "Тестовая статья на русском языке. Проверка функции summarizer."
 
-
-
-
-import pytest
 from django.urls import reverse
 
 @pytest.mark.django_db
